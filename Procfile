7b660abc1ddca9b63554aa808f2cb1a3733db702
cponeill <caseyoneill78@hotmail.com>
2016-08-23 18:39:12 -0400

    moving all code to github

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn get_request.wsgi --log-level=inf --log-file -
---
+++ get_request/views.py
@@ -0,0 +1,65 @@
+from django.http import HttpResponse
+
+from rest_framework.decorators import api_view
+from two1.bitserv.django import payment
+
+import socket
+import json
+import urllib.request as my_request
+
+
+from get_request.status_codes import check_status
+
+@api_view(['GET'])
+@payment.required(0)
+def info(request):
+    get_status_info = '\nCheck the HTTP status: 21 buy http://www.request402.org/get_status?url=example.com\n'
+    get_ip_info = 'Check the IP address of a website: 21 buy http://www.request402.org/get_ip?url=example.com\n'
+    return HttpResponse("You can easily use request402 by running any of the following commands:\n %s%s" % (get_status_info, get_ip_info), status=200)
+
+# Get the header and status code from a website. Output in JSON.
+@api_view(['GET'])
+@payment.required(10)
+def get_status(request):
+
+    """
+    Function calls a website url and returns json output of headers and status code
+
+    Input: => https://www.request402.org/get_status?url=example.com
+    Output: => JSON {status, headers}
+
+    """
+    
+    # Get the website url and assign it to variable url. 
+    website_url = request.GET.get('url')
+    url = 'http://'+website_url
+
+    # Open url, get the status code and headers and assign each to json output.
+    try:
+        response = my_request.urlopen(url)
+        status = response.getcode()
+        description = check_status(status)
+        headers = response.getheaders()[0:8]
+        message = {status : description, 'headers': headers}
+        return HttpResponse(json.dumps(message, indent=2), status=200)
+    except:
+        exception = {"Exception raised" : "Possibly %s doesn't exist" % (url)}
+        return HttpResponse(json.dumps(exception, indent=2))
+
+
+# Get the IP address of a website. Output in JSON
+@api_view(['GET'])
+@payment.required(10)
+def get_ip(request):
+    
+    url = request.GET.get('url')
+    
+    try:
+        response = socket.gethostbyname(url)
+        message = {'ip_address': response, 'url': url}
+        data = json.dumps(message, indent=2)
+        return HttpResponse(data, status=200)
+    except:
+        exception = {"Excpetion raised" : "Possible %s doesn't exist" % (url)}
+        return HttpResponse(json.dumps(exception, indent=2))
+
