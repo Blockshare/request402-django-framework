3975435d295f523a2a1ce60407a9491f91071f75
cponeill <caseyoneill78@hotmail.com>
2016-08-30 17:04:01 -0400

    adding information to /info page.

--- get_request/views.py
+++ get_request/views.py
@@ -14,11 +14,12 @@ import urllib.request as my_request
 @payment.required(0)
 def info(request):
     get_info_border = '-------------------------------------------------------------------------------------------'
-    get_status_info = '\nReturns a websites HTTP status: 21 buy http://www.request402.org/get_status?uri=example.com\n'
-    get_ip_info = 'Returns a websites IP: 21 buy http://www.request402.org/get_ip?uri=example.com\n'
-    get_ip = 'Returns origin IP: 21 buy http://www.request402.org/ip'
-    return HttpResponse("%s\nYou can easily use request402 by running any of the following commands:\n %s%s%s\n%s\n" \
-                         % (get_info_border, get_status_info, get_ip_info, get_ip, get_info_border), status=200)
+    get_status_info = '\nReturns a websites HTTP status: 21 buy https://www.request402.org/get_status?uri=example.com\n'
+    get_ip_info = 'Returns a websites IP: 21 buy https://www.request402.org/get_ip?uri=example.com\n'
+    get_ip = 'Returns origin IP: 21 buy https://www.request402.org/ip\n'
+    get_get = 'Returns GET data: 21 buy https://www.request402.org/get'
+    return HttpResponse("%s\nYou can easily use request402 by running any of the following commands:\n %s%s%s\n%s%s\n" \
+                         % (get_info_border, get_status_info, get_ip_info, get_get, get_ip, get_info_border), status=200)
 
 # Get the header and status code from a website. Output in JSON.
 @api_view(['GET'])
@@ -91,12 +92,10 @@ def ip(request):
 
 
 # Returns the GET Header data. Output is JSON
-# Will work on this once the application is deployed to production.
 @api_view(['GET'])
 @payment.required(10)
 def get(request):
 
-    #return HttpResponse('Soon...', status=200)
     x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
     http_accept = request.META.get('HTTP_ACCEPT')
     http_encoding = request.META.get('HTTP_ACCEPT_ENCODING')
@@ -115,36 +114,6 @@ def get(request):
     except:
         exception = {"Exception": "Something isn't working correctly here."}
         return HttpResponse(json.dumps(exception))
-    """
-    http_accept = request.META.get('HTTP_ACCEPT')
-    http_accept_encoding = request.META.get('HTTP_ACCEPT_ENCODING')
-    http_user_agent = request.META.get('HTTP_USER_AGENT')
-    content_length = request.META.get('CONTENT_LENGTH')
-    content_type = request.META.get('CONTENT_TYPE')
-    server_name = request.META.get('SERVER_NAME')
-    http_host = request.META.get('HTTP_HOST')
-    http_remote_host = request.META.get('REMOTE_HOST')
-    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-
-
-    if http_accept:
-        accept = http_accept.split(',')[0]
-        encoding = http_accept_encoding.split(',')[0]
-        content = content_type.split(',')[0]
-        length = content_length.split(',')[0]
-        agent = http_user_agent.split(',')[0]
-        server = server_name.split(',')[0]
-        host = http_host.split(',')[0]
-        remote_host = http_remote_host.split(',')[0]
-        message = {'Headers': {'Encoding': encoding}}
-        #message = {'Headers': {'Accept': accept, 'Encoding': encoding, 'User-Agent': agent,\
-                   #'Content-Type': content, 'Content-Length': length, 'Server-Name': server,\
-                   #'Host': host, 'Remote-Host': remote_host}}
-        return HttpResponse(json.dumps(message, indent=2), status=200)
-    else:
-        return HttpResponse('NOPE', status=200)
-
-    """
 
 
 # Get JSON-encoded output of a Bitcoin wallet address
