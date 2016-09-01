77d1d071cc402835bbbb2cb088fd508c28bcd7b7
cponeill <caseyoneill78@hotmail.com>
2016-09-01 16:07:30 -0400

    more small changes.

--- get_request/views.py
+++ get_request/views.py
@@ -112,7 +112,7 @@ def get(request):
         encoding = http_encoding.split(',')[0]
         agent = http_user_agent.split(',')[0]
         host = http_host.split(',')[0]
-        org = requests.get(uri + '/' + origin + '/org')
+        org = requests.get(uri + '/' + origin)
         response = {'headers': {'Accept': accept, 'Encoding': encoding, 'User-Agent': agent, \
                     'HTTP-Host': host, 'args': args, 'Org': org}, 'origin': origin}
         return HttpResponse(json.dumps(response, indent=2), status=200)
