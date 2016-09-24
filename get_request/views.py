29778a088398b4afcb80646dd65aef2797aa1fa4
cponeill <caseyoneill78@hotmail.com>
2016-09-24 18:54:11 -0400

    change file names in /templates folder.

--- get_request/views.py
+++ get_request/views.py
@@ -15,8 +15,8 @@ import urllib.request as my_request
 
 
 def index(request):
-    return render(request, '../templates/use_this_for_now.html', status=200)
-    #return render(request, '../templates/index.html', status=200)
+    #return render(request, '../templates/use_this_for_now.html', status=200)
+    return render(request, '../templates/index.html', status=200)
 
 def info(request):
     return render(request, '../templates/info.html', status=200)
