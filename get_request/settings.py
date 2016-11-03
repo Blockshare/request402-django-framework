2e4b3470b83e9c0faaa4b957e2cd866d71463080
cponeill <caseyoneill78@hotmail.com>
2016-11-03 00:22:43 -0400

    updating settings.py file for production.

--- get_request/settings.py
+++ get_request/settings.py
@@ -43,7 +43,7 @@ DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'a5sk!^j5c)i3z^0sqfyz3q+4d%sw4-n07vtr84lb)7ja&9t_km'
+SECRET_KEY = 'a5sk!^j5c)i3z^0sqfyz3q+4d%sw4-n07vtr84lb)7ja&9t_kn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
