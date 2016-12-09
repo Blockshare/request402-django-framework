638b3db31ff9b1f47552a284b4a59a1db71edf70
cponeill <caseyoneill78@hotmail.com>
2016-12-09 15:00:41 -0500

    updating tests with mock.

--- get_request/tests.py
+++ get_request/tests.py
@@ -5,6 +5,8 @@ when you run "manage.py test".
 Replace this with more appropriate tests for your application.
 """
 
+import mock
+
 from django.test import TestCase
 
 # All of these are simple unittests for each endpoint in the web application.
@@ -23,60 +25,115 @@ class SimpleTestCase(TestCase):
         self.assertEqual(resp.status_code, 200)
 
     def test_domain_status(self):
-        resp = self.client.get('/domain_status?uri=google.com')
+        resp = self.client.get('/domain_status?url=google.com')
         self.assertEqual(resp.status_code, 402)
-        self.assertEqual(resp.content, b'"Payment Required"')
+
+    @mock.patch('two1.bitserv.django.decorator.Payment.contains_payment',
+                 return_value=True)
+    def test_domain_buy_status(self, *args):
+        resp = self.client.get('/domain_status?url=google.com')
+        self.assertEqual(resp.status_code, 200)
 
     def test_domain_ip(self):
         resp = self.client.get('/domain_ip?uri=google.com')
         self.assertEqual(resp.status_code, 402)
-        self.assertEqual(resp.content, b'"Payment Required"')
+
+    @mock.patch('two1.bitserv.django.decorator.Payment.contains_payment',
+                 return_value=True)
+    def test_domain_buy_ip(self, *args):
+        resp = self.client.get('/domain_ip?url=google.com')
+        self.assertEqual(resp.status_code, 200)
 
     def test_ip(self):
         resp = self.client.get('/ip')
         self.assertEqual(resp.status_code, 200)
-        self.assertEqual(resp.content, b'{\n  "origin": "127.0.0.1"\n}')
 
     def test_get(self):
         resp = self.client.get('/get')
         self.assertEqual(resp.status_code, 402)
-        self.assertEqual(resp.content, b'"Payment Required"')
+
+    @mock.patch('two1.bitserv.django.decorator.Payment.contains_payment',
+                 return_value=True)
+    def test_buy_get(self, *args):
+        resp = self.client.get('/get')
+        self.assertEqual(resp.status_code, 200)
 
     def test_server_location(self):
         resp = self.client.get('/server-location')
         self.assertEqual(resp.status_code, 402)
-        self.assertEqual(resp.content, b'"Payment Required"')
+
+    @mock.patch('two1.bitserv.django.decorator.Payment.contains_payment',
+                 return_value=True)
+    def test_buy_server_location(self, *args):
+        resp = self.client.get('/server-location')
+        self.assertEqual(resp.status_code, 200)
 
     def test_ssl_cert(self):
         resp = self.client.get('/ssl-cert')
         self.assertEqual(resp.status_code, 402)
 
+    @mock.patch('two1.bitserv.django.decorator.Payment.contains_payment',
+                 return_value=True)
+    def test_buy_ssl_cert(self, *args):
+        resp = self.client.get('/ssl-cert?url=google.com')
+        self.assertEqual(resp.status_code, 200)
+
     def test_ssl_source(self):
         resp = self.client.get('/ssl')
         self.assertEqual(resp.status_code, 402)
-        self.assertEqual(resp.content, b'"Payment Required"')
+
+    @mock.patch('two1.bitserv.django.decorator.Payment.contains_payment',
+                 return_value=True)
+    def test_buy_ssl_source(self, *args):
+        resp = self.client.get('/ssl?url=google.com')
+        self.assertEqual(resp.status_code, 200)
 
     def test_blacklist(self):
-        resp = self.client.get('/blacklist?url=google.com')
+        resp = self.client.get('/blacklist')
         self.assertEqual(resp.status_code, 402)
-        self.assertEqual(resp.content, b'"Payment Required"')
+
+    @mock.patch('two1.bitserv.django.decorator.Payment.contains_payment',
+                 return_value=True)
+    def test_buy_blacklist(self, *args):
+        resp = self.client.get('/blacklist?url=google.com')
+        self.assertEqual(resp.status_code, 200)
 
     def test_ranking(self):
         resp = self.client.get('/domain_rank?url=google.com')
         self.assertEqual(resp.status_code, 402)
-        self.assertEqual(resp.content, b'"Payment Required"')
+
+    @mock.patch('two1.bitserv.django.decorator.Payment.contains_payment',
+                 return_value=True)
+    def test_buy_ranking(self, *args):
+        resp = self.client.get('/domain_rank?url=google.com')
+        self.assertEqual(resp.status_code, 200)
 
     def test_search(self):
-        resp = self.client.get('/domain_search?url=google.com')
+        resp = self.client.get('/domain_search')
         self.assertEqual(resp.status_code, 402)
-        self.assertEqual(resp.content, b'"Payment Required"')
+
+    @mock.patch('two1.bitserv.django.decorator.Payment.contains_payment',
+                 return_value=True)
+    def test_buy_search(self, *args):
+        resp = self.client.get('/domain_search?url=google.com')
+        self.assertEqual(resp.status_code, 200)
 
     def test_social(self):
-        resp = self.client.get('/domain_social_stats?url=google.com')
+        resp = self.client.get('/domain_social_stats')
         self.assertEqual(resp.status_code, 402)
-        self.assertEqual(resp.content, b'"Payment Required"')
+
+    @mock.patch('two1.bitserv.django.decorator.Payment.contains_payment',
+                 return_value=True)
+    def test_buy_social(self, *args):
+        resp = self.client.get('/domain_social_stats?url=google.com')
+        self.assertEqual(resp.status_code, 200)
 
     def test_screenshot(self):
-        resp = self.client.get('/domain_screenshot?url=google.com')
+        resp = self.client.get('/domain_screenshot')
         self.assertEqual(resp.status_code, 402)
-        self.assertEqual(resp.content, b'"Payment Required"')
+
+    @mock.patch('two1.bitserv.django.decorator.Payment.contains_payment',
+                 return_value=True)
+    def test_buy_screenshot(self, *args):
+        resp = self.client.get('/domain_screenshot?url=google.com')
+        self.assertEqual(resp.status_code, 200)
