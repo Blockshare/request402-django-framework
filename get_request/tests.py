21400ae9f08bb2d63ec044bbf75dbe3e3c01e87f
cponeill <caseyoneill78@hotmail.com>
2017-01-17 22:45:40 -0500

    Adding quick comment code.

--- get_request/tests.py
+++ get_request/tests.py
@@ -1,4 +1,12 @@
 """
+Copyright Blockshare Technologies, LLC.
+  ____  _            _     ____  _                      ___ ___  
+ | __ )| | ___   ___| | __/ ___|| |__   __ _ _ __ ___  |_ _/ _ \ 
+ |  _ \| |/ _ \ / __| |/ /\___ \| '_ \ / _` | '__/ _ \  | | | | |
+ | |_) | | (_) | (__|   <  ___) | | | | (_| | | |  __/_ | | |_| |
+ |____/|_|\___/ \___|_|\_\|____/|_| |_|\__,_|_|  \___(_)___\___/ 
+
+
 This file demonstrates writing tests using the unittest module. These will pass
 when you run "manage.py test".
 
